85bf2268a4a5fbdf105bcfca620325601a6b2b61
Zhohfut <413732041@qq.com>
2022-10-29 22:08:20 +0800

    add Q3 code

--- LED/KeilFile/main.c
+++ LED/KeilFile/main.c
@@ -5,63 +5,30 @@
 typedef unsigned char uint8;
 typedef unsigned int uint16;
 
+void Delay100ms(uint8 x);	// 函数声明
+
+uint8 states[] = {0x00};
+
 void main()       
 {
-/*
-	// 版本一：降频插入语句延时（请勿模仿）
-	long a = 10;
-	LED = 0x65;
-	a = 10*a;		// 乘法语句的执行速度慢于加法
-	LED = ~LED;		// 将所有位翻转
-	a = 10*a;
-	LED = ~LED;
-	a = 10*a;
-	LED = ~LED;
-	a++;
-	LED = ~LED;
-	a++;
-	LED = ~LED;
-*/
-	// 版本二：循环结构
-/*
-	// 不断闪烁
-	uint8 i, j;
+	LED = 0xFE;
 	while (1) {
-		LED = ~LED;
-		i = 255;
-		j = 255;
-		while (j--)
-			while (i--);
-	}
-*/
-	// 指定次数
 
-	// 死循环方式
-	uint8 count = 0;
-	uint8 i, j;
-	while (1) {
-		LED = ~LED;		
-		i = 255;
-		j = 255;
-		while (j--)			// 延时
-			while (i--);
-		count++;			// 判断次数
-		if (count == 5) {
-			break;
-		}
 	}
-/*
-	// 条件控制方式
-	uint8 count = 5;
-	uint8 i, j;
-	while (count--) {
-		LED = ~LED;
-		i = 255;
-		j = 255;
-		while (j--)
-			while (i--);
-	}
-*/
-	while(1);
 }
+
+void Delay100ms(uint8 x)		// 函数定义，@12.000MHz
+{
+	uint8 i, j;
+	while (x--) {
+		// STC-ISP自动生成的代码
+		i = 195;
+		j = 138;
+		do
+		{
+			while (--j);
+		} while (--i);
+	}
+}
+
 
